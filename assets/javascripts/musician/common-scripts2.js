9678ce724a9b31de052a0cbc589ebc4f6ea640d4
Allen <shotzara@naver.com>
2017-06-12 17:26:04 +0900

    0.3.4

---
+++ assets/javascripts/musician/common-scripts2.js
@@ -0,0 +1,12 @@
+
+// custom bar chart
+
+    if ($(".custom-bar-chart")) {
+        $(".bar").each(function () {
+            var i = $(this).find(".value").html();
+            $(this).find(".value").html("");
+            $(this).find(".value").animate({
+                height: i
+            }, 2000)
+        })
+    }
